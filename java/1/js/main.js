46d338251e10ecccdbc8c09ac8e17b484fe9a669
jacka55 <jekotandashvili@gmail.com>
2022-01-30 18:47:20 +0400

    new

---
+++ java/1/js/main.js
@@ -0,0 +1,25 @@
+let weekday = prompt("შეიყვანე კვირის დღე")
+if(weekday == "ორშაბათი"){
+    console.log("შემოყვანილია სამუშაო დღე")
+}
+else if(weekday == "სამშაბათი"){
+    console.log("შემოყვანილია სამუშაო დღე")
+}
+else if(weekday == "ოთხშაბათი"){
+    console.log("შემოყვანილია სამუშაო დღე")
+}
+else if(weekday == "ხუთშაბათი"){
+    console.log("შემოყვანილია სამუშაო დღე")
+}
+else if(weekday == "პარასკევი"){
+    console.log("შემოყვანილია სამუშაო დღე")
+}
+else if(weekday == "შაბათი"){
+    console.log("შემოყვანილია დასვენების დღე")
+}
+else if(weekday == "კვირა"){
+    console.log("შემოყვანილია დასვენების დღე")
+}
+else{
+    console.log("შეცდომა")
+}
